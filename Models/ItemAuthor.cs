6f0512b0a634ec6c2fa0df0caf9db1638def73c7
giannivanlooij <54837793+giannivanlooij@users.noreply.github.com>
2023-09-06 12:11:15 +0200

    Add project files.

---
+++ Models/ItemAuthor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Models
+{
+    public class ItemAuthor
+    {
+        public int Id { get; set; }
+
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+
+        public int ItemId { get; set; }
+        public Item Item { get; set; }
+    }
+}
---
+++ Models/ItemGenre.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Models
+{
+    public class ItemGenre
+    {
+        public int Id { get; set; }
+        public int GenreId { get; set; }
+        public Genre Genre { get; set; }
+
+        public int ItemId { get; set; }
+        public Item Item { get; set; }
+    }
+}
