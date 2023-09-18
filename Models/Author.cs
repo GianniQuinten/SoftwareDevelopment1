c8026c7414d3409adce6001e4c9f1aef1bd808b7
QuintenSchaap <quinten.schaap@student.rocvf.nl>
2023-09-18 17:07:37 +0200

    Tried to make the crud it is not working yet but hopefully soon

--- Models/Author.cs
+++ Models/Author.cs
@@ -1,20 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Library.Models
 {
-    public class Author
+    public class Author : INotifyPropertyChanged
     {
-        public int Id { get; set; }
+        private int id;
+        private string name;
+        private string lastname;
 
-        public string Name { get; set; }
+        public int Id 
+        { 
+            get => id; set
+            {
+                id = value;
+                OnPropertyChanged(nameof(Id));  
+            }
+        }
 
-        public string Lastname { get; set; }
+        public string Name 
+        { 
+            get => name; set
+            {
+                name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
 
-        public ICollection<ItemAuthor> ItemAuthors { get; set; }
+        public string Lastname
+        { 
+            get => lastname; set
+            {
+                lastname = value;
+                OnPropertyChanged(nameof(Lastname));
+            }
+        }
 
+    public ICollection<ItemAuthor> ItemAuthors { get; set; }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
---
+++ ViewModels/AuthorViewModel.cs
@@ -0,0 +1,40 @@
+﻿using SoftwareDevelopment1.Views;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareDevelopment1.ViewModels
+{
+    public class AuthorViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private ObservableCollection<AuthorDetails>  _lstAuthorDetails;
+
+        public ObservableCollection<AuthorDetails> LstAuthorDetails
+        {
+            get { return _lstAuthorDetails; }
+            set
+            {
+                _lstAuthorDetails = value;
+                OnPropertyChanged(nameof(LstAuthorDetails));
+            }
+        }
+        
+        AuthorE
+        public AuthorViewModel()
+        {
+
+        }
+    }
+}
