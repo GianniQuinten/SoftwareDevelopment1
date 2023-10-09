c42d80fc1700cfaeb7e319f54c8dd770be1bc7d2
giannivanlooij <54837793+giannivanlooij@users.noreply.github.com>
2023-10-09 12:18:10 +0200

    made a barbone placeholder

---
+++ Views/AddItem.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SoftwareDevelopment1.Views
+{
+	/// <summary>
+	/// Interaction logic for AddItem.xaml
+	/// </summary>
+	public partial class AddItem : Window
+	{
+		public AddItem()
+		{
+			InitializeComponent();
+		}
+	}
+}
